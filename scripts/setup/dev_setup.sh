7529f86c2f914fcf74774a8f4007a5424bfda4e4
zsy-98 <2021104304@ruc.edu.cn>
2024-01-21 10:03:33 +0800

    monitor CPU time of query

--- scripts/setup/dev_setup.sh
+++ scripts/setup/dev_setup.sh
@@ -216,9 +216,6 @@ function install_protobuf {
 		;;
 	*)
 		arch=$(uname -m)
-		x86_64
-		https://gitee.com/irascible-brother/protobuf/releases/tag/v3.15.8
-		https://gitee.com/irascible-brother/protobuf/releases/download/v3.15.8/protoc-3.15.8-linux-x86_64.zip
 		PB_REL="https://gitee.com/irascible-brother/protobuf/releases"
 		curl -LO $PB_REL/download/v3.15.8/protoc-3.15.8-linux-${arch}.zip
 		unzip protoc-3.15.8-linux-${arch}.zip -d protoc-3.15.8
